6157bb82a37503a2c3aee2c91d88237be51230d3
Dylan Gagnon <dylan.gagnon3@hotmail.com>
2025-03-11 16:05:16 -0400

    V1 menuManager

--- menuManager.cpp
+++ menuManager.cpp
@@ -1,3 +1,221 @@
-#include "menuManager.h"
+#include "MenuManager.h"
+#include <QKeyEvent>
+#include <QDebug>
 
-menuManager::menuManager() {}
+menuManager::menuManager(QWidget *parent)
+    : QWidget(parent), stackedWidget(new QStackedWidget(this)),
+    serialComm(new SerialCommunication(this)) {
+
+    setupMenus();
+
+    connect(serialComm, &SerialCommunication::dataReceived, this, &menuManager::handleArduinoInput);
+    serialComm->startReading();
+}
+
+menuManager::~menuManager() {
+    delete stackedWidget;
+    serialComm->stopReading();
+    delete serialComm;
+}
+
+void menuManager::setupMenus() {
+    // Initialisation des menus
+    mainWindow = new MainWindow();
+    choixJoueur = new ChoixJoueur();
+    map = new Map();
+    commande = new Commande();
+    regle = new Regle();
+    capture = new Capture();
+    combat = new Combat();
+    genidex = new Genidex();
+    historencontre = new HistoRencontre();
+
+    // Ajout des menus dans le QStackedWidget
+    stackedWidget->addWidget(mainWindow);
+    stackedWidget->addWidget(choixJoueur);
+    stackedWidget->addWidget(map);
+    stackedWidget->addWidget(commande);
+    stackedWidget->addWidget(regle);
+    stackedWidget->addWidget(capture);
+    stackedWidget->addWidget(combat);
+    stackedWidget->addWidget(genidex);
+    stackedWidget->addWidget(historencontre);
+
+    // Définir la taille du widget de manière appropriée
+    stackedWidget->setGeometry(0, 0, 1280, 720); // Ajuster la taille selon l'UI
+
+    // Associer les signaux pour chaque menu, si nécessaire
+    // Exemple : connect(mainWindow, SIGNAL(menuChanged(int)), this, SLOT(handleMenuChange(int)));
+
+    setLayout(new QVBoxLayout());
+    layout()->addWidget(stackedWidget);
+}
+
+
+void menuManager::switchToMenu(QWidget *menu) {
+    stackedWidget->setCurrentWidget(menu);
+
+    activeMenu = stackedWidget->currentWidget()->objectName();
+    qDebug() << "Widget actif : " << activeMenu;
+}
+
+
+
+void menuManager::handleArduinoInput(const QJsonObject &jsonObject) {
+    // Extraire les états des boutons à partir du JSON
+    stateBouton1 = jsonObject["button1"].toBool();
+    stateBouton2 = jsonObject["button2"].toBool();
+    stateBouton3 = jsonObject["button3"].toBool();
+    stateBouton4 = jsonObject["button4"].toBool();
+    stateBoutonJoy = jsonObject["bouttonJoy"].toBool();
+    stateJoy = jsonObject["JoyPosition"].toString();
+    accX = jsonObject["AccX"].toInt();
+    accY = jsonObject["AccY"].toInt();
+    accZ = jsonObject["AccZ"].toInt();
+
+    if (activeMenu == "mainWindow"){
+        if (stateBouton1) {
+            switchToMenu(mainWindow);
+        } else if (stateBouton2) {
+            switchToMenu(choixJoueur);
+        } else if (stateBouton3) {
+            switchToMenu(regle);
+        } else if (stateBouton4) {
+            QApplication::quit();
+        }
+    }else if (activeMenu == "choixJoueur"){
+
+    }else if (activeMenu == "map"){
+
+    }else if (activeMenu == "commande"){
+
+    }else if (activeMenu == "regle"){
+
+    }else if (activeMenu == "capture"){
+
+    }else if (activeMenu == "combat"){
+
+    }else if (activeMenu == "genidex"){
+
+    }else if (activeMenu == "historencontre"){
+
+    }
+}
+
+
+
+void menuManager::keyPressEvent(QKeyEvent *event) {
+    switch (event->key()) {
+    case Qt::Key_1:
+        if (activeMenu == "mainWindow"){
+            switchToMenu(mainWindow);
+        }else if (activeMenu == "choixJoueur"){
+
+        }else if (activeMenu == "map"){
+
+        }else if (activeMenu == "commande"){
+
+        }else if (activeMenu == "regle"){
+
+        }else if (activeMenu == "capture"){
+
+        }else if (activeMenu == "combat"){
+
+        }else if (activeMenu == "genidex"){
+
+        }else if (activeMenu == "historencontre"){
+
+        }
+
+        break;
+    case Qt::Key_2:
+        if (activeMenu == "mainWindow"){
+            switchToMenu(choixJoueur);
+        }else if (activeMenu == "choixJoueur"){
+
+        }else if (activeMenu == "map"){
+
+        }else if (activeMenu == "commande"){
+
+        }else if (activeMenu == "regle"){
+
+        }else if (activeMenu == "capture"){
+
+        }else if (activeMenu == "combat"){
+
+        }else if (activeMenu == "genidex"){
+
+        }else if (activeMenu == "historencontre"){
+
+        }
+        break;
+    case Qt::Key_3:
+        if (activeMenu == "mainWindow"){
+            switchToMenu(regle);
+        }else if (activeMenu == "choixJoueur"){
+
+        }else if (activeMenu == "map"){
+
+        }else if (activeMenu == "commande"){
+
+        }else if (activeMenu == "regle"){
+
+        }else if (activeMenu == "capture"){
+
+        }else if (activeMenu == "combat"){
+
+        }else if (activeMenu == "genidex"){
+
+        }else if (activeMenu == "historencontre"){
+
+        }
+        break;
+    case Qt::Key_4:
+        if (activeMenu == "mainWindow"){
+            QApplication::quit();
+        }else if (activeMenu == "choixJoueur"){
+
+        }else if (activeMenu == "map"){
+
+        }else if (activeMenu == "commande"){
+
+        }else if (activeMenu == "regle"){
+
+        }else if (activeMenu == "capture"){
+
+        }else if (activeMenu == "combat"){
+
+        }else if (activeMenu == "genidex"){
+
+        }else if (activeMenu == "historencontre"){
+
+        }
+        break;
+    case Qt::Key_Escape:
+        if (activeMenu == "mainWindow"){
+
+        }else if (activeMenu == "choixJoueur"){
+
+        }else if (activeMenu == "map"){
+
+        }else if (activeMenu == "commande"){
+
+        }else if (activeMenu == "regle"){
+
+        }else if (activeMenu == "capture"){
+
+        }else if (activeMenu == "combat"){
+
+        }else if (activeMenu == "genidex"){
+
+        }else if (activeMenu == "historencontre"){
+
+        }
+        break;
+    default:
+        QWidget::keyPressEvent(event);
+        break;
+    }
+}
+
+
--- menuManager.h
+++ menuManager.h
@@ -2,11 +2,62 @@
 #define MENUMANAGER_H
 
 #include <QWidget>
+#include <QStackedWidget>
+#include "serialcommunication.h"
 
-class menuManager
-{
+#include "MainWindow.h"
+#include "choixjoueur.h"
+#include "Map.h"
+#include "commande.h"
+#include "regle.h"
+#include "capture.h"
+#include "combat.h"
+#include "genidex.h"
+#include "historencontre.h"
+
+class menuManager : public QWidget {
+    Q_OBJECT
 public:
-    menuManager();
+    explicit menuManager(QWidget *parent = nullptr);
+    ~menuManager();
+private:
+    void setupMenus();
+    void switchToMenu(QWidget *menu);
+    void handleArduinoInput(const QJsonObject &jsonObject);
+    void keyPressEvent(QKeyEvent *event);
+
+    // Gestion des menus
+    QStackedWidget *stackedWidget;
+
+    // Menus spécifiques
+    MainWindow *mainWindow;
+    ChoixJoueur *choixJoueur;
+    Map *map;
+    Commande *commande;
+    Regle *regle;
+    Capture *capture;
+    Combat *combat;
+    Genidex *genidex;
+    HistoRencontre *historencontre;
+
+    SerialCommunication *serialComm;
+
+    // Variables pour les boutons de la manette
+    bool stateBouton1 = false;
+    bool stateBouton2 = false;
+    bool stateBouton3 = false;
+    bool stateBouton4 = false;
+    bool stateBoutonJoy = false;
+    QString stateJoy = "default";
+    QString lastStateJoy = "default";
+    int accX = 0;
+    int accY = 0;
+    int accZ = 0;
+
+    // Variable autres
+    QString activeMenu;
+
+
 };
 
 #endif // MENUMANAGER_H
